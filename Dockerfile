db28d965a5f6931f8f01998e1d17c60ee17675f0
srinathLN7 <srinath0393@gmail.com>
2024-01-18 08:23:12 +0100

    change port to 80

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,16 @@
 # Use the official Golang base image
 FROM golang:1.21-alpine
 
+ENV PORT="80"
+
 # Set the working directory inside the container
 WORKDIR /app
 
 # Copy the application source code into the container
 COPY . .
 
-EXPOSE 8080
+
+EXPOSE 80
 
 # Build the Go application
 RUN go build -o main .
--- main.go
+++ main.go
@@ -5,22 +5,43 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
-const port = 8080
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello AWS!\n")
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
 }
 
-type HealthResponse struct {
-	Status string `json:"status"`
+func handler(w http.ResponseWriter, r *http.Request) {
+	var msg = "Hello ci-cd-test server"
+
+	response := Response{
+		Status:  "OK",
+		Message: msg,
+	}
+
+	// Marshal the struct to JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Set Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+
+	// Write the JSON response with a 200 status code
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonResponse)
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a HealthResponse struct
-	response := HealthResponse{
-		Status: "OK",
+	// Create a Response struct
+	response := Response{
+		Status:  "OK",
+		Message: "healthy",
 	}
 
 	// Marshal the struct to JSON
@@ -39,11 +60,22 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	var p = os.Getenv("PORT")
+	if len(p) == 0 {
+		p = "8080"
+	}
+
+	port, err := strconv.Atoi(p)
+	if err != nil {
+		panic("port not yet set")
+	}
+
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/health", healthHandler)
 
-	fmt.Printf("server is listening on port %d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	fmt.Printf("ci-cd-test server listening on port %d\n", port)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		log.Fatal("error:", err)
 	}
